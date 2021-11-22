c8a1c2b211a2d074c92eea18dd0f685973a23385
V1PeRI4 <V1PeRI4@DESKTOP-C59PJAE>
2021-11-22 21:24:02 +0300

    Добавьте файлы проекта.

---
+++ func2_komar/func2_komar.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int FarlToYard(double);
+
+
+int main()
+{
+	double dist, finDist;
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите расстояние в фарлонах: ";
+	cin >> dist;
+	finDist = FarlToYard(dist);
+	cout << "Расстояние в ярдах: " << finDist << endl;
+	return 0;
+	
+}
+
+int FarlToYard(double sts) 
+{
+	return 220 * sts;
+}
